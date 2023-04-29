e65e547b3be5b34393e6f1f9acaf9bac20162c9d
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-04-29 19:56:48 +0900

    merge rec and clf

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import joblib
 import json
 from pydantic import BaseModel
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
 
 app = FastAPI()
 
@@ -36,8 +38,39 @@ class clf_input(BaseModel):
     성별구분코드_M:int
 
 
-@app.post('/grade_pred')
-def grade_pred(input_parameters : clf_input):
+
+def non_rec():
+
+
+    # loading the saved model
+    create_CF = joblib.load('./model/rec/non/create_CF.pkl')
+    get_CF = joblib.load('./model/rec/non/get_CF.pkl')
+    data = joblib.load('./model/rec/user_group/adult_group_{}.pkl'.format(group_num))
+    get_freq_item = joblib.load('./model/rec/non/get_freq_item.pkl')
+    get_association_rules = joblib.load('./model/rec/non/get_association_rules.pkl')
+    get_apriori_result = joblib.load('./model/rec/non/get_apriori_result.pkl')
+    get_sorted= joblib.load('./model/rec/non/get_sorted.pkl')
+    create_top5_ex = joblib.load('./model/rec/non/create_top5_ex.pkl')
+    get_sparse_matrix = joblib.load('./model/rec/non/get_sparse_matrix.pkl')
+    get_top5_ex = joblib.load('./model/rec/non/get_top5_ex.pkl')
+
+    similarity_pair=create_CF(data, new_user_df)
+    pre_ex_list =get_CF(similarity_pair, "준비운동", int(data.shape[0]*(10/100)))
+    main_ex_list = get_CF(similarity_pair, "본운동", int(data.shape[0]*(10/100)))
+    after_ex_list = get_CF(similarity_pair, "마무리운동", int(data.shape[0]*(10/100)))
+
+
+    pre_ar_df = get_apriori_result(pre_ex_list,min_support = 0.05, min_confidence=0.8)
+    pre_top5 = get_top5_ex(pre_ar_df)
+
+    main_ar_df = get_apriori_result(main_ex_list,min_support = 0.05, min_confidence=0.8)
+    main_top5 = get_top5_ex(main_ar_df)
+    
+    after_ar_df = get_apriori_result(after_ex_list,min_support = 0.05, min_confidence=0.8)
+    after_top5 = get_top5_ex(after_ar_df)
+
+
+def gred_pred(input_parameters : clf_input):
     input_data = input_parameters.json()
     input_dictionary = json.loads(input_data)
 
@@ -64,6 +97,13 @@ def grade_pred(input_parameters : clf_input):
     
     prediction = grade_clf_model.predict([input_list])
     return prediction[0].item()
+     
+
+@app.post('/non_rec')
+def non_rec(input_parameters : clf_input):
+    group_num = gred_pred(input_parameters)
+    return group_num
+
 
 
 @app.get('/')
