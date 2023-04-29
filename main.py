e88fbab1a6c6d7de06808d454e1db3b4c890b40c
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-04-29 19:38:50 +0900

    directory

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import joblib
 import json
-from .BaseModel import clf_input 
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -19,7 +19,21 @@ app.add_middleware(
 
 
 
-
+class clf_input(BaseModel):
+    
+    측정연령수 : int
+    신장 : float
+    체중 : float
+    체지방율: float
+    앉아윗몸앞으로굽히기 : float
+    BMI : float
+    교차윗몸일으키기 :  float
+    왕복오래달리기 : int
+    왕복달리기_10M_4회:float
+    제자리_멀리뛰기:float
+    상대악력:float
+    성별구분코드_F:int
+    성별구분코드_M:int
 
 
 @app.post('/grade_pred')
